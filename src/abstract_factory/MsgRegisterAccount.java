282dd8c49c2f9a289bb3332a2064ed73e1ba9f0d
mateusmatosleonardo <m4teusmatos@gmail.com>
2025-06-17 10:02:13 -0400

    add MsgRegisterClient and MsgRegisterAccount class

---
+++ src/abstract_factory/MsgRegisterAccount.java
@@ -0,0 +1,7 @@
+package abstract_factory;
+
+public class MsgRegisterAccount {
+  public String toString() {
+    return "Mensagem de Registro de Conta";
+  }
+}
---
+++ src/abstract_factory/MsgRegisterClient.java
@@ -0,0 +1,7 @@
+package abstract_factory;
+
+public class MsgRegisterClient {
+  public String toString() {
+    return "Mensagem de Registro de Cliente";
+  }
+}
